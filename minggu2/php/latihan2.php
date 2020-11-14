68d5315baeea01e1d6707fd60436b6e4f73b9df4
Muhammad Ichsan Al Fatih <muhammad.14116126@student.itera.ac.id>
2020-11-14 20:29:47 +0700

    Tugas dan Latihan

---
+++ minggu2/php/latihan2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+    <?php
+        $data = array("lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat");
+        sort($data);
+
+        echo "Mengurutkan data:";
+        echo "<br>";
+        print_r ($data);
+    ?>
+</body>
+</html>
